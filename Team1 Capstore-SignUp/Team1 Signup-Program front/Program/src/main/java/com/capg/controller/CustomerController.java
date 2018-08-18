51abd0c41717f53d76ad137f68cd0e36850776bd
deepak6034 <40890293+deepak6034@users.noreply.github.com>
2018-08-18 17:43:46 +0530

    Update CustomerController.java

--- Team1 Capstore-SignUp/Team1 Signup-Program front/Program/src/main/java/com/capg/controller/CustomerController.java
+++ Team1 Capstore-SignUp/Team1 Signup-Program front/Program/src/main/java/com/capg/controller/CustomerController.java
@@ -1,19 +1,29 @@
 package com.capg.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.capg.bean.Customer;
+import com.capg.bean.Merchant;
+
 
 
 @RestController
 public class CustomerController {
 	@RequestMapping("/customer")
-	public Customer getProduct(@RequestParam("id") String id,@RequestParam("password") String password) {
+	public Customer getProduct(@RequestParam String id,@RequestParam String password) {
 		RestTemplate rt=new RestTemplate();
-		Customer c=rt.getForObject("http://localhost:4448/savecustomers?id="+id+"&password="+password, Customer.class);
+		Customer c=rt.getForObject("http://localhost:4441/savecustomers?id="+id+"&password="+password, Customer.class);
 		return c;
 }
+	@RequestMapping("/merchant")
+    public Merchant getMerchant(@RequestParam String id,@RequestParam String password) {
+    RestTemplate rt1=new RestTemplate();
+	Merchant m=rt1.getForObject("http://localhost:4441/savemerchant?id="+id+"&password="+password, Merchant.class);
+	return m;
+
+	}
 }
