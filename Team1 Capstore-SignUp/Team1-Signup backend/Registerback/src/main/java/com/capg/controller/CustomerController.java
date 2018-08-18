f8b7c66ebcc67c792d06e2ae8ab7c84bbcf7b348
deepak6034 <40890293+deepak6034@users.noreply.github.com>
2018-08-18 17:45:57 +0530

    Update CustomerController.java

--- Team1 Capstore-SignUp/Team1-Signup backend/Registerback/src/main/java/com/capg/controller/CustomerController.java
+++ Team1 Capstore-SignUp/Team1-Signup backend/Registerback/src/main/java/com/capg/controller/CustomerController.java
@@ -1,14 +1,17 @@
 package com.capg.controller;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 import com.capg.service.CustomerService;
+import com.capg.service.MerchantService;
 
 
 
@@ -16,12 +19,20 @@ import com.capg.service.CustomerService;
 public class CustomerController {
 	@Autowired
 	CustomerService service;
+	@Autowired
+	MerchantService service1;
 
-
-	@RequestMapping("/savecustomers/{id}/{password}")
-	public void saveCustomer(@PathVariable String id,@PathVariable String password){
+	@RequestMapping(value="/savecustomers{id}{password}")
+	public void saveCustomer(@RequestParam String id,@RequestParam String password){
 	service.saveCustomer(id,password);	 
 		 	 }
+	
+	@RequestMapping(value="/savemerchant{id}{password}")
+	public void saveMerchant(@RequestParam String id,@RequestParam String password){
+	service1.saveMerchant(id,password);	 
+		 	 }
 
 		 	 }
 
+
+	
